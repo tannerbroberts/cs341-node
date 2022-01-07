ace6f414d0ad94a89da13f020c099273f3c97bbc
Tanner Roberts <tannerbroberts@gmail.com>
2022-01-07 16:27:46 -0700

    sound cut out in udemy, pc restart...

--- section03/server.js
+++ section03/server.js
@@ -15,8 +15,19 @@ const http = require('http'); // auto-adds .js at the end
 // Create the server with the listener function defined as ananonamous function
 //*****************************************************************************
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Pave</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+    res.write('</html>');
+    res.end();
+    // Can't write any more after this!!!
 });
 
 // Tells the newly creates server to listen, and the information it needs to listen
 server.listen(5000);
+
+// Note: The event loop never closes unless there are no registered listeners
+// Note: process.exit() quits the server hard exits, like break from a for loop
