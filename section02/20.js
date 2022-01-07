6c4dfc3ebc85464a35ebd149d5ba322a628f23b5
Tanner Roberts <tannerbroberts@gmail.com>
2022-01-07 15:25:55 -0700

    delete21

--- section02/20.js
+++ section02/20.js
@@ -1,15 +1,23 @@
 // Crutial concept!!!
 
-// Async code (callback function)
+// Async code (returns a promise)
 const fetchData = () => {
+    // When resolved, this new promise will wait 1.5 sec., then call the 'resolve' callback function passed in as the first parameter
+    // Promises typically have much more complicated logic that determines weather or not they call the resolve or reject methods
     const promise = new Promise((resolve, reject) => {
+        // calling fetchData() creates and returns this promise (runs this code)
+        // This code waits for 1.5 sec, then calls resolve, but it doesn't wait 1.5 seconds to return, it does that immediately.
         setTimeout(() => {
-            resolve('Done!');
-        }, 1500);
+            resolve('fetchDate() promise Done!');
+        }, 500);
     });
+    // This happens pretty close to immediately.
     return promise;
 }
 
+// This gets called when we run our file
+// It waits 1.5 seconds to log 'First timer is done!'
+// It then calls fetchData(), which runs, and returns a promise
 setTimeout(() => {
     console.log('First timer is done!');
     fetchData()
@@ -19,6 +27,10 @@ setTimeout(() => {
         })
         .then(text2 => {
             console.log(text2);
+            return fetchData();
+        })
+        .then(text3 => {
+            console.log(text3);
         });
 }, 1500);
 
