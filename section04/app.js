1490e4cb63ebc1902c4b20865bde82d8110f0131
Tanner Roberts <tannerbroberts@gmail.com>
2022-01-10 13:23:40 -0700

    Completes section04

--- section04/app.js
+++ section04/app.js
@@ -1,1 +1,11 @@
-// npm install <package> --save-dev downloads and installs into a single project but only for development purposes
+// Note: npm install <package> --save-dev downloads and installs into a single project but only for development purposes
+
+const http = require('http');
+
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
