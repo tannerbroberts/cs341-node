97fab8d40b5ae224bc029c88a4888ec5ec508166
Tanner Roberts <tannerbroberts@gmail.com>
2022-01-12 12:43:00 -0700

    housekeeping

--- store-project/app.js
+++ store-project/app.js
@@ -1,17 +1,17 @@
-const path = require('path');
-
+// Imports
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
-
+// Controllers
 const errorController = require('./controllers/error');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
