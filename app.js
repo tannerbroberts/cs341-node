496529bf76489dfa86b6cbfe0ef8727367dc3639
Tanner Roberts <tannerbroberts@gmail.com>
2022-01-13 19:51:01 -0700

    re-structures the file system

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
+  User.findById('61e0ddafbcf3d30e875ca0c8')
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
