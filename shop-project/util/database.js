70617a0715f9ecc85e4aa5659ec59d7a81077376
Tanner Roberts <tannerbroberts@gmail.com>
2022-01-13 19:03:19 -0700

    Adds personal database password

--- shop-project/util/database.js
+++ shop-project/util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://root:run@nodecluster.cjoru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
